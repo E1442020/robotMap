f1e9810843876a452e112bbc7d3b6287a2f3b048
Eman Masoud <114053062+E1442020@users.noreply.github.com>
2023-12-06 13:53:30 +0200

    update

--- src/MyComponent.tsx
+++ src/MyComponent.tsx
@@ -1,3 +1,74 @@
+// import React, { useEffect, useState } from "react";
+// import { GoogleMap, useJsApiLoader, Marker } from "@react-google-maps/api";
+
+// const containerStyle = {
+//   width: "700px",
+//   height: "500px",
+// };
+
+// // const center = {
+// //     lat :30.040904864466672, lng:31.209323080460027
+// // };
+
+// function MyComponent() {
+//   const { isLoaded } = useJsApiLoader({
+//     id: "google-map-script",
+//     googleMapsApiKey: "AIzaSyDJBumFjSoH-7NL9545OiwwS8iF-Mb_LW0",
+//   });
+//   const [currentLocation, setCurrentLocation] = useState<any>(null);
+
+//   const [map, setMap] = React.useState<any>(null);
+
+//   useEffect(() => {
+//     if (navigator.geolocation) {
+//       navigator.geolocation.getCurrentPosition(
+//         (position) => {
+//           const { latitude, longitude } = position.coords;
+//           setCurrentLocation({ lat: latitude, lng: longitude });
+//         },
+//         (error) => {
+//           console.error("Error getting user location:", error);
+//         },
+//         { enableHighAccuracy: true, timeout: 5000, maximumAge: 0 }
+//       );
+//     } else {
+//       console.error("Geolocation is not supported by this browser.");
+//     }
+//   }, []);
+
+//   useEffect(() => {
+//     if (isLoaded && currentLocation && map) {
+//       const bounds = new window.google.maps.LatLngBounds(currentLocation);
+//       map.fitBounds(bounds);
+//     }
+//   }, [isLoaded, currentLocation, map]);
+
+//   const onLoad = React.useCallback(function callback(map: any) {
+//     setMap(map);
+//   }, []);
+
+//   const onUnmount = React.useCallback(function callback() {
+//     setMap(null);
+//   }, []);
+
+//   return isLoaded ? (
+//     <GoogleMap
+//       mapContainerStyle={containerStyle}
+//       center={currentLocation}
+//       zoom={1}
+//       onLoad={onLoad}
+//       onUnmount={onUnmount}
+//     >
+//       {/* Child components, such as markers, info windows, etc. */}
+//       <>{currentLocation && <Marker position={currentLocation} />}</>
+//     </GoogleMap>
+//   ) : (
+//     <div>Map loading error</div>
+//   );
+// }
+
+// export default React.memo(MyComponent);
+
 import React, { useEffect, useState } from "react";
 import { GoogleMap, useJsApiLoader, Marker } from "@react-google-maps/api";
 
@@ -6,22 +77,17 @@ const containerStyle = {
   height: "500px",
 };
 
-// const center = {
-//     lat :30.040904864466672, lng:31.209323080460027
-// };
-
 function MyComponent() {
   const { isLoaded } = useJsApiLoader({
     id: "google-map-script",
-    googleMapsApiKey: "AIzaSyDJBumFjSoH-7NL9545OiwwS8iF-Mb_LW0",
+    googleMapsApiKey: "AIzaSyDJBumFjSoH-7NL9545OiwwS8iF-Mb_LW0", // Replace with your Google Maps API key
   });
   const [currentLocation, setCurrentLocation] = useState<any>(null);
-
-  const [map, setMap] = React.useState<any>(null);
+  const [map, setMap] = useState<any>(null);
 
   useEffect(() => {
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
+      const watchId = navigator.geolocation.watchPosition(
         (position) => {
           const { latitude, longitude } = position.coords;
           setCurrentLocation({ lat: latitude, lng: longitude });
@@ -31,17 +97,14 @@ function MyComponent() {
         },
         { enableHighAccuracy: true, timeout: 5000, maximumAge: 0 }
       );
+
+      return () => {
+        navigator.geolocation.clearWatch(watchId);
+      };
     } else {
       console.error("Geolocation is not supported by this browser.");
     }
   }, []);
-
-  useEffect(() => {
-    if (isLoaded && currentLocation && map) {
-      const bounds = new window.google.maps.LatLngBounds(currentLocation);
-      map.fitBounds(bounds);
-    }
-  }, [isLoaded, currentLocation, map]);
 
   const onLoad = React.useCallback(function callback(map: any) {
     setMap(map);
@@ -51,28 +114,23 @@ function MyComponent() {
     setMap(null);
   }, []);
 
-  //   const onLoad = React.useCallback(function callback(map) {
-  //     // This is just an example of getting and using the map instance!!! don't just blindly copy!
-  //     const bounds = new window.google.maps.LatLngBounds(currentLocation);
-  //     map.fitBounds(bounds);
-
-  //     setMap(map)
-  //   }, [])
-
-  //   const onUnmount = React.useCallback(function callback(map) {
-  //     setMap(null)
-  //   }, [])
+  useEffect(() => {
+    if (isLoaded && currentLocation && map) {
+      const bounds = new window.google.maps.LatLngBounds();
+      bounds.extend(currentLocation);
+      map.fitBounds(bounds);
+    }
+  }, [isLoaded, currentLocation, map]);
 
   return isLoaded ? (
     <GoogleMap
       mapContainerStyle={containerStyle}
       center={currentLocation}
-      zoom={1}
+      zoom={15} // Adjust the zoom level as per your needs
       onLoad={onLoad}
       onUnmount={onUnmount}
     >
-      {/* Child components, such as markers, info windows, etc. */}
-      <>{currentLocation && <Marker position={currentLocation} />}</>
+      {currentLocation && <Marker position={currentLocation} />}
     </GoogleMap>
   ) : (
     <div>Map loading error</div>
